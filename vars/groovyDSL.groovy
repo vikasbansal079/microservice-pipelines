a93a02621201b2c57edaf1c9fcf136a36ec7ef76
Vikas Bansal <vikasbansal079@gmail.com>
2020-04-21 11:21:16 +0530

    updated

--- vars/groovyDSL.groovy
+++ vars/groovyDSL.groovy
@@ -1,7 +1,9 @@
 def call(body) {
 
   // evaluate the body block, and collect configuration into the object
+
   def config = [:]
+  print config
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
